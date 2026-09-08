02ee756c54462fa10f7db6b502dacdb3faa7d62b
Fabian Faedrich <fabian.faedrich@rfh-koeln.de>
2026-09-08 13:36:13 +0200

    .env setup

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-// app.set('view engine', 'pug');
-// app.use(express.static(__dirname + '/public'));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+ app.set('view engine', 'pug');
+ app.use(express.static(__dirname + '/public'));
+ app.use(express.urlencoded({ extended: true }));
+ app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -22,12 +22,13 @@ const PRIVATE_APP_ACCESS = '';
 
 // * Code for Route 3 goes here
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
 
-* * App.get sample
+//* * This is sample code to give you a reference for how you should structure your calls. 
+
+//* * App.get sample
 app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    console.log("chack" + PRIVATE_APP_ACCESS)
+        const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -41,6 +42,7 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
+/** 
 * * App.post sample
 app.post('/update', async (req, res) => {
     const update = {
@@ -68,4 +70,4 @@ app.post('/update', async (req, res) => {
 
 
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3040, () => console.log('Listening on http://localhost:3040'));
